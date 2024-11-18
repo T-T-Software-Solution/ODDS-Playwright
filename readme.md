3522c3bffc1d34f8a174e5226de37b52b9f8e86a
Ponggun <wittawat@tt-ss.net>
2024-11-18 14:48:45 +0700

    Hello World

---
+++ readme.md
@@ -0,0 +1,37 @@
+Install Yarn 
+```
+brew install yarn
+```
+
+Create Playwright
+https://playwright.dev/docs/intro#installing-playwright
+```
+yarn create playwright
+```
+
+Select Typescript, and install Playwright browser
+
+ลองรัน Hello World ด้วย
+```
+yarn playwright test
+```
+
+ลองเปิด HTML Report 
+```
+ yarn playwright show-report
+ ```
+
+Run UI
+ ```
+ yarn playwright test --ui
+ ```
+
+ Run Debugger
+ ```
+ yarn playwright test --debug
+ ```
+
+ Codegen
+ ```
+ yarn playwright codegen https://workshop-playwright.vercel.app
+ ```
---
+++ tests/odds.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('Login Success', async ({ page }) => {
+  await page.goto('https://workshop-playwright.vercel.app/');
+  await page.getByTestId('username-input').click();
+  await page.getByTestId('username-input').fill('john');
+  await page.getByTestId('password-input').click();
+  await page.getByTestId('password-input').fill('doe');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByText('Welcome to ODDS')).toBeVisible();
+});
+
+test('Miss Password', async ({ page }) => {
+    await page.goto('https://workshop-playwright.vercel.app/');
+    await page.getByTestId('username-input').click();
+    await page.getByTestId('username-input').fill('');
+    await page.getByTestId('username-input').press('CapsLock');
+    await page.getByTestId('username-input').fill('admin');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByText('Password is required')).toBeVisible();
+  });
